3afb4f689b09ad30973ddaea1caaf49edf7bd39e
Victor <unique_wq@hotmail.com>
2019-02-20 22:13:40 +1100

    step 5 commit

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -95,7 +95,8 @@
 // 	}
 // }
 
-import { NavigationMixin } from 'lightning/navigation';
+import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
+import { fireEvent } from 'c/pubsub';
 import { loadStyle } from 'lightning/platformResourceLoader';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 import { LightningElement, track, wire } from 'lwc';
@@ -103,8 +104,18 @@ import { LightningElement, track, wire } from 'lwc';
 import searchBears from '@salesforce/apex/BearController.searchBears';
 export default class BearListNav extends NavigationMixin(LightningElement) {
 	@track searchTerm = '';
+	// @wire(searchBears, {searchTermText: '$searchTerm'})
+	// bears;
+	@track bears;
+	@wire(CurrentPageReference) pageRef;
 	@wire(searchBears, {searchTermText: '$searchTerm'})
-	bears;
+	loadBears(result) {
+		this.bears = result;
+		if (result.data) {
+			fireEvent(this.pageRef, 'bearListUpdate', result.data);
+		}
+	}
+
 	connectedCallback() {
 		loadStyle(this, ursusResources + '/style.css');
 	}
